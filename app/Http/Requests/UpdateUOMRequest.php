2749fabd02b21e007feea3a05e393a0d898a1e53
mbunico <mark.unico@aclcbutuan.edu.ph>
2024-08-07 15:45:24 +0800

    Update UpdateUOMRequest.php

--- app/Http/Requests/UpdateUOMRequest.php
+++ app/Http/Requests/UpdateUOMRequest.php
@@ -11,7 +11,7 @@ class UpdateUOMRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,11 @@ class UpdateUOMRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'group_id' => 'exists:setup_uom_group,id',
+            'name' => 'required|string|max:255',
+            'symbol' => 'required|string|max:10',
+            'conversion' => 'nullable|numeric',
+            'is_standard' => 'boolean|required'
         ];
     }
 }
