cea3a2b40fb9a641651db1591aad68809c1ae020
mbunico <mark.unico@aclcbutuan.edu.ph>
2024-08-07 15:03:48 +0800

    models

---
+++ app/Models/UOM.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class UOM extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $table = 'setup_uom';
+    protected $fillable = [
+        'id',
+        'group_id',
+        'name',
+        'symbol',
+        'conversion',
+        'is_standard',
+    ];
+
+}
