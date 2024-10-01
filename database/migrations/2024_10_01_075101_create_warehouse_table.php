fb7dbea024c4a597b8e03c3a0aa9bb7d8955fe6e
mbunico <mark.unico@aclcbutuan.edu.ph>
2024-10-01 15:53:08 +0800

    Create Warehouse table

---
+++ database/migrations/2024_10_01_075101_create_warehouse_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('warehouse', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('owner_id')->nullable();
+            $table->string('name');
+            $table->string('location')->nullable();
+            $table->enum('owner_type', ['project', 'department']);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('warehouse');
+    }
+};
