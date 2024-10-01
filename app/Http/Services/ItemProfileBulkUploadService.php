e7a830a04d888a92dcac3fea00e80cdbbb16122b
mbunico <mark.unico@aclcbutuan.edu.ph>
2024-10-01 09:22:27 +0800

    Update ItemProfile Bulk Upload Error message

--- app/Http/Services/ItemProfileBulkUploadService.php
+++ app/Http/Services/ItemProfileBulkUploadService.php
@@ -3,64 +3,73 @@
 namespace App\Http\Services;
 
 use App\Models\ItemProfile;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
+use App\Models\UOM;
+use App\Models\UOMGroup;
 
 class ItemProfileBulkUploadService
 {
     public function parseCsv(array $rows)
     {
         $header = [
-            'Item Description', 'Thickness', 'Thickness UOM', 'Length', 'Length UOM',
-            'Width', 'Width UOM', 'Height', 'Height UOM', 'Outside Diameter',
-            'Outside Diameter UOM', 'Inside Diameter', 'Inside Diameter UOM', 'Volume',
-            'Volume UOM', 'Specification', 'Grade', 'Color', 'UOM', 'Item Group',
-            'Sub Item Group', 'Inventory Type'
+            'Item Description',
+            'Thickness',
+            'Thickness UOM',
+            'Length',
+            'Length UOM','Width',
+            'Width UOM',
+            'Height',
+            'Height UOM',
+            'Outside Diameter','Outside Diameter UOM',
+            'Inside Diameter',
+            'Inside Diameter UOM',
+            'Volume',
+            'Volume UOM',
+            'Specification',
+            'Grade',
+            'Color',
+            'UOM',
+            'Item Group',
+            'Sub Item Group',
+            'Inventory Type'
         ];
 
         $processed = [];
         $duplicates = [];
         $unprocessed = [];
 
-        $validUOMs = DB::table('setup_uom')
-            ->whereNull('deleted_at')
-            ->pluck('symbol', 'name')
-            ->toArray();
+        $validUOMGroup = UOMGroup::get();
+        $validUOMs = UOM::get();
 
-        $validUOMGroup = DB::table('setup_uom_group')
-            ->whereNull('deleted_at')
-            ->pluck('name')
-            ->toArray();
-
-        $dataRows = array_slice($rows, 1);
+        $dataRows = array_slice($rows, 1); // Skip header row
 
         foreach ($dataRows as $row) {
             if (count($row) == count($header)) {
                 $data = array_combine($header, $row);
 
+                // Initialize the data structure for the row
                 $filteredData = [
-                    'item_description' => $data['Item Description'] ?? null,
-                    'thickness_val' => $data['Thickness'] ?? null,
-                    'thickness_uom' => $data['Thickness UOM'] ?? null,
-                    'length_val' => $data['Length'] ?? null,
-                    'length_uom' => $data['Length UOM'] ?? null,
-                    'width_val' => $data['Width'] ?? null,
-                    'width_uom' => $data['Width UOM'] ?? null,
-                    'height_val' => $data['Height'] ?? null,
-                    'height_uom' => $data['Height UOM'] ?? null,
-                    'outside_diameter_val' => $data['Outside Diameter'] ?? null,
-                    'outside_diameter_uom' => $data['Outside Diameter UOM'] ?? null,
-                    'inside_diameter_val' => $data['Inside Diameter'] ?? null,
-                    'inside_diameter_uom' => $data['Inside Diameter UOM'] ?? null,
-                    'volume_val' => $data['Volume'] ?? null,
-                    'volume_uom' => $data['Volume UOM'] ?? null,
-                    'specification' => $data['Specification'] ?? null,
-                    'grade' => $data['Grade'] ?? null,
-                    'color' => $data['Color'] ?? null,
-                    'uom' => $data['UOM'] ?? null,
-                    'item_group' => $data['Item Group'] ?? null,
-                    'sub_item_group' => $data['Sub Item Group'] ?? null,
-                    'inventory_type' => $data['Inventory Type'] ?? null,
+                    'item_description' => ['value' => $data['Item Description'] ?? null],
+                    'thickness_val' => ['value' => $data['Thickness'] ?? null],
+                    'thickness_uom' => ['value' => $data['Thickness UOM'] ?? null],
+                    'length_val' => ['value' => $data['Length'] ?? null],
+                    'length_uom' => ['value' => $data['Length UOM'] ?? null],
+                    'width_val' => ['value' => $data['Width'] ?? null],
+                    'width_uom' => ['value' => $data['Width UOM'] ?? null],
+                    'height_val' => ['value' => $data['Height'] ?? null],
+                    'height_uom' => ['value' => $data['Height UOM'] ?? null],
+                    'outside_diameter_val' => ['value' => $data['Outside Diameter'] ?? null],
+                    'outside_diameter_uom' => ['value' => $data['Outside Diameter UOM'] ?? null],
+                    'inside_diameter_val' => ['value' => $data['Inside Diameter'] ?? null],
+                    'inside_diameter_uom' => ['value' => $data['Inside Diameter UOM'] ?? null],
+                    'volume_val' => ['value' => $data['Volume'] ?? null],
+                    'volume_uom' => ['value' => $data['Volume UOM'] ?? null],
+                    'specification' => ['value' => $data['Specification'] ?? null],
+                    'grade' => ['value' => $data['Grade'] ?? null],
+                    'color' => ['value' => $data['Color'] ?? null],
+                    'uom' => ['value' => $data['UOM'] ?? null],
+                    'item_group' => ['value' => $data['Item Group'] ?? null],
+                    'sub_item_group' => ['value' => $data['Sub Item Group'] ?? null],
+                    'inventory_type' => ['value' => $data['Inventory Type'] ?? null],
                 ];
 
                 $requiredFields = [
@@ -83,64 +92,82 @@ class ItemProfileBulkUploadService
                     'outside_diameter_uom', 'inside_diameter_uom', 'volume_uom'
                 ];
 
+                $uomGroups = [
+                    'uom'
+                ];
+
                 $isUnprocessed = false;
-                $errorMessages = [];
 
+                // Check required fields
                 foreach ($requiredFields as $field) {
-                    if (empty($filteredData[$field])) {
+                    if (empty($filteredData[$field]['value'])) {
+                        $filteredData[$field]['error'] = "Missing or null required field: $field";
                         $isUnprocessed = true;
-                        $errorMessages[] = "Missing or null required field: $field";
                     }
                 }
 
-                if (!$isUnprocessed) {
-                    $atLeastOneSpecFilled = false;
-                    foreach ($specificationFields as $field) {
-                        if (!empty($filteredData[$field])) {
-                            $atLeastOneSpecFilled = true;
-                        }
+                // Ensure at least one specification field is filled
+                $atLeastOneSpecFilled = false;
+                foreach ($specificationFields as $field) {
+                    if (!empty($filteredData[$field]['value'])) {
+                        $atLeastOneSpecFilled = true;
                     }
+                }
+                if (!$atLeastOneSpecFilled) {
+                    $filteredData['specification']['error'] = "At least one specification field must be filled";
+                    $isUnprocessed = true;
+                }
 
-                    if (!$atLeastOneSpecFilled) {
+                // Validate numeric fields
+                foreach ($numericFields as $field) {
+                    if (!empty($filteredData[$field]['value']) && !is_numeric($filteredData[$field]['value'])) {
+                        $filteredData[$field]['error'] = "Field $field must be numeric";
                         $isUnprocessed = true;
-                        $errorMessages[] = "At least one specification field must be filled";
                     }
                 }
 
-                if (!$isUnprocessed) {
-                    foreach ($numericFields as $field) {
-                        if (!empty($filteredData[$field]) && !is_numeric($filteredData[$field])) {
-                            $isUnprocessed = true;
-                            $errorMessages[] = "Field $field must be numeric";
-                        }
-                    }
-                }
-
+                // Validate UOM fields
                 foreach ($uomFields as $uomField) {
-                    if (!empty($filteredData[$uomField])) {
-                        $uomValue = $filteredData[$uomField];
-                        // Check if UOM is valid by either 'name' or 'symbol'
-                        if (!in_array($uomValue, $validUOMs) && !array_key_exists($uomValue, $validUOMs)) {
+
+                    if (!empty($filteredData[$uomField]['value'])) {
+                        $uomValue = $filteredData[$uomField]['value'];
+                        $isValid = $validUOMs->filter(function ($uom) use ($uomValue) {
+                            return $uom->symbol == $uomValue;
+                        })->first();
+
+                        if (is_null($isValid)) {
+                            $filteredData[$uomField]['error'] = "The value: $uomValue is not found in $uomField field.";
                             $isUnprocessed = true;
-                            $errorMessages[] = "The value: $uomValue is not found in $uomField field.";
+                        } else {
+                            $filteredData[$uomField]['uom_id'] = $isValid->id;
                         }
                     }
                 }
 
-                if (!empty($filteredData['uom'])) {
-                    $mainUomValue = $filteredData['uom'];
-                    // Check if UOM group is valid
-                    if (!in_array($mainUomValue, $validUOMGroup)) {
-                        $isUnprocessed = true;
-                        $errorMessages[] = "Invalid input: $mainUomValue in UOM field.";
+                foreach ($uomGroups as $uomGroup) {
+
+                    if (!empty($filteredData[$uomGroup]['value'])) {
+                        $uomValue = $filteredData[$uomGroup]['value'];
+                        $isValid = $validUOMGroup->filter(function ($uom) use ($uomValue) {
+                            return $uom->name == $uomValue;
+                        })->first();
+
+                        if (is_null($isValid)) {
+                            $filteredData[$uomGroup]['error'] = "The value: $uomValue is not found in $uomField field.";
+                            $isUnprocessed = true;
+                        } else {
+                            $filteredData[$uomGroup]['uom_group_id'] = $isValid->id;
+                        }
                     }
                 }
 
+                // $filteredData['sku'] = $this->generateSKU($filteredData);
+
+                // Add to unprocessed if there are errors, otherwise check for duplicates
                 if ($isUnprocessed) {
-                    $filteredData['errors'] = $errorMessages;
                     $unprocessed[] = $filteredData;
                 } else {
-                    $existingItem = ItemProfile::where('item_description', $filteredData['item_description'])->first();
+                    $existingItem = ItemProfile::where('item_description', $filteredData['item_description']['value'])->first();
                     if ($existingItem) {
                         $duplicates[] = $filteredData;
                     } else {
@@ -156,4 +183,5 @@ class ItemProfileBulkUploadService
 
         return [$processed, $duplicates, $unprocessed];
     }
+
 }
