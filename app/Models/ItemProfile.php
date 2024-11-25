6cdf3a36713d3560789216deb1995bc16a3eeaeb
mbunico <mark.unico@aclcbutuan.edu.ph>
2024-11-25 15:31:15 +0800

    Update: New Item Profile -> Fields Restructure

--- app/Models/ItemProfile.php
+++ app/Models/ItemProfile.php
@@ -57,76 +57,20 @@ class ItemProfile extends Model
      * MODEL ATTRIBUTES
      * ==================================================
      */
-    public function getThicknessAttribute()
-    {
-        $value = $this->thickness_val;
-        $uom = $this->thicknessUom ? $this->thicknessUom->name : $this->thickness_uom;
 
+    public function getUomSymbolsAttribute()
+    {
         return [
-            'full' => "{$value} {$uom}",
-            'abbreviated' => "{$value} " . ($this->thicknessUom ? $this->thicknessUom->symbol : $this->thickness_uom)
+            'thickness_uom_symbol' => $this->thicknessUom ? $this->thicknessUom->symbol : $this->thickness_uom,
+            'length_uom_symbol' => $this->lengthUom ? $this->lengthUom->symbol : $this->length_uom,
+            'width_uom_symbol' => $this->widthUom ? $this->widthUom->symbol : $this->width_uom,
+            'height_uom_symbol' => $this->heightUom ? $this->heightUom->symbol : $this->height_uom,
+            'outside_diameter_uom_symbol' => $this->outsideDiameterUom ? $this->outsideDiameterUom->symbol : $this->outside_diameter_uom,
+            'inside_diameter_uom_symbol' => $this->insideDiameterUom ? $this->insideDiameterUom->symbol : $this->inside_diameter_uom,
+            'volume_uom_symbol' => $this->volumeUom ? $this->volumeUom->symbol : $this->volume_uom,
         ];
     }
-    public function getLengthAttribute()
-    {
-        $value = $this->length_val;
-        $uom = $this->lengthUom ? $this->lengthUom->name : $this->length_uom;
 
-        return [
-            'full' => "{$value} {$uom}",
-            'abbreviated' => "{$value} " . ($this->lengthUom ? $this->lengthUom->symbol : $this->length_uom)
-        ];
-    }
-    public function getWidthAttribute()
-    {
-        $value = $this->width_val;
-        $uom = $this->widthUom ? $this->widthUom->name : $this->width_uom;
-
-        return [
-            'full' => "{$value} {$uom}",
-            'abbreviated' => "{$value} " . ($this->widthUom ? $this->widthUom->symbol : $this->width_uom)
-        ];
-    }
-    public function getHeightAttribute()
-    {
-        $value = $this->height_val;
-        $uom = $this->heightUom ? $this->heightUom->name : $this->height_uom;
-
-        return [
-            'full' => "{$value} {$uom}",
-            'abbreviated' => "{$value} " . ($this->heightUom ? $this->heightUom->symbol : $this->height_uom)
-        ];
-    }
-    public function getOutsideDiameterAttribute()
-    {
-        $value = $this->outside_diameter_val;
-        $uom = $this->outsideDiameterUom ? $this->outsideDiameterUom->name : $this->outside_diameter_uom;
-
-        return [
-            'full' => "{$value} {$uom}",
-            'abbreviated' => "{$value} " . ($this->outsideDiameterUom ? $this->outsideDiameterUom->symbol : $this->outside_diameter_uom)
-        ];
-    }
-    public function getInsideDiameterAttribute()
-    {
-        $value = $this->inside_diameter_val;
-        $uom = $this->insideDiameterUom ? $this->insideDiameterUom->name : $this->inside_diameter_uom;
-
-        return [
-            'full' => "{$value} {$uom}",
-            'abbreviated' => "{$value} " . ($this->insideDiameterUom ? $this->insideDiameterUom->symbol : $this->inside_diameter_uom)
-        ];
-    }
-    public function getVolumeAttribute()
-    {
-        $value = $this->volume_val;
-        $uom = $this->volumeUom ? $this->volumeUom->name : $this->volume_uom;
-
-        return [
-            'full' => "{$value} {$uom}",
-            'abbreviated' => "{$value} " . ($this->volumeUom ? $this->volumeUom->symbol : $this->volume_uom)
-        ];
-    }
     public function getUomFullNameAttribute()
     {
         $uom = $this->uomName ? $this->uomName->name : 'null';
---
+++ database/migrations/2024_11_25_053935_drop_foreign_keys_in_item_profile_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('item_profile', function (Blueprint $table) {
+            $table->dropForeign(['thickness_uom']);
+            $table->dropForeign(['length_uom']);
+            $table->dropForeign(['width_uom']);
+            $table->dropForeign(['height_uom']);
+            $table->dropForeign(['outside_diameter_uom']);
+            $table->dropForeign(['inside_diameter_uom']);
+            $table->dropForeign(['volume_uom']);
+            $table->dropForeign(['uom_conversion_group_id']);
+            $table->dropColumn(['thickness_uom', 'length_uom', 'width_uom', 'height_uom', 'outside_diameter_uom', 'inside_diameter_uom', 'volume_uom', 'uom_conversion_group_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('item_profile', function (Blueprint $table) {
+            $table->foreignId('thickness_uom')->nullable()->constrained('setup_uom');
+            $table->foreignId('length_uom')->nullable()->constrained('setup_uom');
+            $table->foreignId('width_uom')->nullable()->constrained('setup_uom');
+            $table->foreignId('height_uom')->nullable()->constrained('setup_uom');
+            $table->foreignId('outside_diameter_uom')->nullable()->constrained('setup_uom');
+            $table->foreignId('inside_diameter_uom')->nullable()->constrained('setup_uom');
+            $table->foreignId('volume_uom')->nullable()->constrained('setup_uom');
+            $table->foreignId('uom_conversion_group_id')->nullable()->constrained('setup_uom_group');
+        });
+    }
+};
