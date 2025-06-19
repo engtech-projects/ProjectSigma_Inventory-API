99b87cce08010cfc0bd1129f0305eb5daaca4b94
Jose Janlofre Dy <35553424+janlofredy@users.noreply.github.com>
2025-06-19 17:47:03 +0800

    Update 2025_06_16_062339_create_price_quotations_table.php

--- database/migrations/2025_06_16_062339_create_price_quotations_table.php
+++ database/migrations/2025_06_16_062339_create_price_quotations_table.php
@@ -13,7 +13,7 @@ return new class () extends Migration {
         Schema::create('price_quotations', function (Blueprint $table) {
             $table->id();
             $table->foreignId('request_procurement_id')
-                  ->constrained('request_procurement')
+                  ->constrained('request_procurements')
                   ->cascadeOnUpdate()
                   ->restrictOnDelete();
             $table->foreignId('supplier_id')
