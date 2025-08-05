6c0f0502ae5e2c257beb8a53d04a974c8d7332fe
mark <116787082+mbunico@users.noreply.github.com>
2025-08-05 11:09:16 +0800

    Canvass Summary Database Schema and Model

---
+++ app/Http/Controllers/RequestCanvassSummaryItemsController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\RequestCanvassSummaryItems;
+use Illuminate\Http\Request;
+
+class RequestCanvassSummaryItemsController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function show(RequestCanvassSummaryItems $requestCanvassSummaryItems)
+    {
+       //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function destroy(RequestCanvassSummaryItems $requestCanvassSummaryItems)
+    {
+        //
+    }
+}
