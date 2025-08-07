d1b68e8bfc1b4e29741c6c2ced51e2f7d76056e9
mark <116787082+mbunico@users.noreply.github.com>
2025-08-07 13:12:47 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -198,7 +198,6 @@ Route::middleware('auth:api')->group(function () {
             ->only(['update']);
         Route::prefix('canvass-summary')->group(function () {
             Route::resource('resource', RequestCanvassSummaryController::class)->names("requestCanvassSummary");
-            Route::get('all-request', [RequestCanvassSummaryController::class, 'allRequests']);
             Route::get('my-request', [RequestCanvassSummaryController::class, 'myRequests']);
             Route::get('my-approvals', [RequestCanvassSummaryController::class, 'myApprovals']);
             Route::get('{requestCanvassSummary}', [RequestCanvassSummaryController::class, 'show']);
