2e7a548c8b945c0321eda3785ed713e2b0e1b863
mark <116787082+mbunico@users.noreply.github.com>
2025-08-12 09:43:20 +0800

    Update PriceQuotationForCanvassResource.php

--- app/Http/Resources/PriceQuotationForCanvassResource.php
+++ app/Http/Resources/PriceQuotationForCanvassResource.php
@@ -26,7 +26,6 @@ class PriceQuotationForCanvassResource extends JsonResource
             'reference_no' => $this->requestProcurement?->requisitionSlip?->reference_no,
             'quotation_no' => $this->quotation_no,
             'quotation_date' => $this->created_at_date_human,
-            'total_amount' => $this->total_amount,
             'grand_total' => $this->grand_total_amount,
             'items' => PriceQuotationItemDetailedResource::collection($this->items),
         ];
