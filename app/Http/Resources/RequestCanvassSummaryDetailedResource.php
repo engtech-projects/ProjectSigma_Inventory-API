045a03acea927f10fa53789616cb74d8390904f2
mark <116787082+mbunico@users.noreply.github.com>
2025-08-15 01:02:16 +0800

    Update RequestCanvassSummaryDetailedResource.php

--- app/Http/Resources/RequestCanvassSummaryDetailedResource.php
+++ app/Http/Resources/RequestCanvassSummaryDetailedResource.php
@@ -22,9 +22,16 @@ class RequestCanvassSummaryDetailedResource extends JsonResource
             'availability' => $this->availability,
             'delivery_terms' => $this->delivery_terms,
             'remarks' => $this->remarks,
-            'price_quotation' => $this->priceQuotation,
-            'supplier' => $this->priceQuotation->supplier,
-            'items' => $this->items,
+            'price_quotation_id' => $this->priceQuotation->id,
+            'supplier' => $this->priceQuotation->supplier->only('id', 'company_name', 'company_address', 'contact_person_name', 'contact_person_number'),
+            'items' => $this->priceQuotation->items
+                ->whereIn(
+                    'item_id',
+                    $this->items->pluck('item_id')->all()
+                )
+                ->values()
+                ->toArray(),
+
         ];
     }
 }
