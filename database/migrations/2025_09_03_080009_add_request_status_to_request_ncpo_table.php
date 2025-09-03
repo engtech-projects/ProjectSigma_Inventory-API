6d7650a4b4a8c6605c09f192aa1432e1c4a0d5f0
mark <116787082+mbunico@users.noreply.github.com>
2025-09-03 16:02:52 +0800

    Create 2025_09_03_080009_add_request_status_to_request_ncpo_table.php

---
+++ database/migrations/2025_09_03_080009_add_request_status_to_request_ncpo_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Enums\RequestStatuses;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('request_ncpos', function (Blueprint $table) {
+            $table->enum('request_status', RequestStatuses::toArray())->after('approvals')->default(RequestStatuses::PENDING);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('request_ncpo', function (Blueprint $table) {
+            $table->dropColumn('request_status');
+        });
+    }
+};
