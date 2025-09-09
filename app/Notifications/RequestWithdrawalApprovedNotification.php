04c677b678e65711395995031f77354c0cdc0adb
Ricky Canonigo <rickycanonigo58@gmail.com>
2025-09-09 22:06:21 +0800

    Update RequestWithdrawalApprovedNotification.php
    Fixed the spelling

--- app/Notifications/RequestWithdrawalApprovedNotification.php
+++ app/Notifications/RequestWithdrawalApprovedNotification.php
@@ -9,7 +9,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class RequestWithdrawalApprovalNotification extends Notification
+class RequestWithdrawalApprovedNotification extends Notification
 {
     use Queueable;
 
