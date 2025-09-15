9357b1c6e70085857a45ff8adb10109b4aab12e6
Jose Janlofre Dy <janlofredy@gmail.com>
2025-09-15 17:34:31 +0800

    Persist enum value, not enum object

--- app/Http/Controllers/RequestSupplierController.php
+++ app/Http/Controllers/RequestSupplierController.php
@@ -90,7 +90,7 @@ class RequestSupplierController extends Controller
     public function store(StoreRequestSupplier $request)
     {
         $validated = $request->validated();
-        $validated['request_status'] = RequestStatuses::PENDING;
+        $validated['request_status'] = RequestStatuses::PENDING->value;
         $validated['created_by'] = auth()->user()->id;
         $requestSupplier = DB::transaction(function () use ($validated, $request) {
             $requestSupplier = RequestSupplier::create(
