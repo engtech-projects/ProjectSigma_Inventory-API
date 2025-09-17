b8bbc0c62ac583aceea80eaaed92cdaf56e66c11
mark <116787082+mbunico@users.noreply.github.com>
2025-09-17 15:32:36 +0800

    Enhance PO resources with NCPO and item change details

--- app/Models/RequestProcurement.php
+++ app/Models/RequestProcurement.php
@@ -72,4 +72,15 @@ class RequestProcurement extends Model
             'id'
         );
     }
+    public function purchaseOrders()
+    {
+        return $this->hasManyThrough(
+            RequestPurchaseOrder::class,
+            RequestCanvassSummary::class,
+            'price_quotation_id',
+            'request_canvass_summary_id',
+            'id',
+            'id'
+        );
+    }
 }
