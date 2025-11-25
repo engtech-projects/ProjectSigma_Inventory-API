b2089739aadff8ddaddecd55fe83673ef5eb14a4
mark <116787082+mbunico@users.noreply.github.com>
2025-11-25 15:07:15 +0800

    Update 2025_11_21_021209_update_approval_columns_on_request_turnovers_table.php

--- database/migrations/2025_11_21_021209_update_approval_columns_on_request_turnovers_table.php
+++ database/migrations/2025_11_21_021209_update_approval_columns_on_request_turnovers_table.php
@@ -3,42 +3,29 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class () extends Migration {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::table('request_turnovers', function (Blueprint $table) {
             $table->dropColumn(['approved_by', 'approval_status']);
         });
-
-        Schema::table('request_turnovers', function (Blueprint $table) {
-            $table->renameColumn('requested_by', 'created_by');
-        });
-
+        DB::statement('ALTER TABLE request_turnovers CHANGE `requested_by` `created_by` bigint unsigned NOT NULL');
         Schema::table('request_turnovers', function (Blueprint $table) {
             $table->json('approvals')->nullable()->after('created_by');
             $table->string('request_status')->nullable()->after('approvals');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::table('request_turnovers', function (Blueprint $table) {
             $table->dropColumn(['approvals', 'request_status']);
         });
-
+        DB::statement('ALTER TABLE request_turnovers CHANGE `created_by` `requested_by` bigint unsigned NOT NULL');
         Schema::table('request_turnovers', function (Blueprint $table) {
-            $table->renameColumn('created_by', 'requested_by');
-        });
-
-        Schema::table('request_turnovers', function (Blueprint $table) {
-            $table->string('approved_by')->nullable()->after('requested_by');
+            $table->bigInteger('approved_by')->unsigned()->nullable()->after('requested_by');
             $table->enum('approval_status', ['Pending', 'Approved', 'Rejected'])
                   ->default('Pending')
                   ->after('approved_by');
