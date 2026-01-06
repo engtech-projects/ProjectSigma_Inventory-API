89dd1944e5586241b24f90ff309522297826ed82
mark <116787082+mbunico@users.noreply.github.com>
2026-01-06 15:35:48 +0800

    run pint

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,7 +7,6 @@ use App\Models\SetupDepartments;
 use App\Models\SetupEmployees;
 use App\Models\SetupProjects;
 use App\Models\SetupWarehouses;
-use App\Models\TransactionMaterialReceiving;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
